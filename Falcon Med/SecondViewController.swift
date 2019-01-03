f41ab51cd06fbfaeb984d1010a2a4dc87f6b56a8
Shawn Patel <shawnpatel8@att.net>
2019-01-02 19:04:55 -0800

    Initial Commit

---
+++ Falcon Med/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Falcon Med
+//
+//  Created by Shawn Patel on 1/2/19.
+//  Copyright © 2019 Shawn Patel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
