618f90474b9324693831893486799c498b021d2d
Shawn Patel <shawnpatel8@att.net>
2019-01-02 21:38:04 -0800

    Configured FlightViewController

--- Falcon Med/FlightViewController.swift
+++ Falcon Med/FlightViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class FlightViewController: UIViewController {
 
+    @IBOutlet weak var agree: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func beginFlight(_ sender: UIButton) {
-        tabBarController?.tabBar.isHidden = true
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
         
-        let takeoffTime = Int(NSDate().timeIntervalSince1970)
-        UserDefaults.standard.set(takeoffTime, forKey: "takeoffTime")
+        agree.isOn = false
+    }
+    
+    @IBAction func beginFlight(_ sender: UIBarButtonItem) {
+        if agree.isOn {
+            tabBarController?.tabBar.isHidden = true
+        
+            let takeoffTime = Int(NSDate().timeIntervalSince1970)
+            UserDefaults.standard.set(takeoffTime, forKey: "takeoffTime")
+            
+            self.performSegue(withIdentifier: "flightToAvionics", sender: self)
+        } else {
+            let alertController = UIAlertController(title: "Agree to the Terms", message: "The flight cannot begin until you agree to follow the terms displayed.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func getHelp(_ sender: UIBarButtonItem) {
+        if let url = NSURL(string: "https://www.faa.gov/uas") {
+            UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)
+        }
     }
 }
